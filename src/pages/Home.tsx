e27bf6f75b93400caa00dcd0f7ea5545fe1d7e53
Abiodun-Omoniyi Victory <104084524+preciousvictory@users.noreply.github.com>
2024-12-23 18:43:32 +0100

    initial setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// Landing Page
+
+const Home = () => {
+    return (
+        <div className="kanit-medium">
+            <h1>Mementosg</h1>
+        </div>
+    )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+      },
+    },
   },
   plugins: [],
-}
+};
