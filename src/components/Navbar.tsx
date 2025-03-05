c0517e45dfa6c52c654d91088cc1ae610d152f9e
Abiodun-Omoniyi Victory <104084524+preciousvictory@users.noreply.github.com>
2025-03-05 17:28:57 +0100

    Landing page correction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import HamburgerDropdownMenu from "./ui/HamburgerDropdownMenu"
 
 const Navigation = () => {
     return (
-        <div className="bg-[#F5F5F5] ">
+        <div >
             <div className="flex-row flex justify-between items-center w-full px-8 pt-9 pb-2 ">
                 <Link to={'/'} >
                     <div className="">
