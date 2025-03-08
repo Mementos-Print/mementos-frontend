6d3723529c28ee696c5a01ccb31537fd3c34bc66
Abiodun-Omoniyi Victory <104084524+preciousvictory@users.noreply.github.com>
2025-03-08 23:17:29 +0100

    blank print

---
+++ src/components/Blank Print/EditorCanvas.tsx
@@ -0,0 +1,9 @@
+const EditorCanvas = () => {
+    return (
+      <div className="canvasbox">
+        <canvas width="100%" height="50%"></canvas>
+      </div>
+    );
+  };
+  
+  export default EditorCanvas;
--- src/components/form/blankprint/EditPictures.tsx
+++ src/components/form/blankprint/EditPictures.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ChevronLeft, ChevronRight } from "../../../assets/icons/Icon";
+import * as fabric from 'fabric';
 
 const EditPictures = ({ selectedImages }: { selectedImages: File[] }) => {
    const [currentImage, setCurrentImage] = useState(0)
@@ -21,11 +22,18 @@ const EditPictures = ({ selectedImages }: { selectedImages: File[] }) => {
       }
    }
 
+   const canvasRef = useRef<React.MutableRefObject<null> | null>(null);
+   useEffect(() => {
+      canvasRef.current = new fabric.Canvas('fabric-canvas');
+    }, []);
+
    return (
       <div className="h-full flex flex-col justify-center items-center align-middle">
          {selectedImages.length == 0 ?
             <div className="text-lg text-disabledText kanit-light">Select Images to print.</div> :
             <div className="w-1/2 flex flex-row justify-center gap-6 items-center">
+               <img id="my-image" src={URL.createObjectURL(selectedImages[currentImage])} style={{display: 'none'}}/>
+               <canvas id="c" width="100%" height="60%"></canvas>
                <div className="previous p-2 bg-gradient-to-r from-gradient_from to-disabled rounded-full w-fit h-fit cursor-pointer"
                   onClick={() => handlePrevImage()}>
                   <ChevronLeft />
